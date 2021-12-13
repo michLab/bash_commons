2a527ea4a5de1951f3388276f3669482a412f2bd
Michal Labowski <m.labowski@wb.com.pl>
2021-12-13 20:21:36 +0100

    feat: Add colored print

---
+++ print.sh
@@ -0,0 +1,33 @@
+BLACK="\e[0;30m"
+BLUE="\e[0;34m"
+GREEN="\e[0;32m"
+CYAN="\e[0;36m"
+RED="\e[0;31m"
+PURPLE="\e[0;35m"
+YELLOW="\e[0;33m"
+RESET_FFORMAT="\e[m"
+
+BOLD="\e[1m"
+
+function print_color() {
+    color=$1
+    text=$2
+    echo -e "${color}${text}${RESET_FFORMAT}"
+}
+
+function print_success() {
+    print_color "$GREEN" "$1"
+}
+
+function print_error() {
+    print_color "$RED" "$1"
+}
+
+function print_warning() {
+    print_color "$YELLOW" "$1"
+}
+
+function print_bold() {
+    echo -e "${BOLD}$1${RESET_FFORMAT}"
+}
+
